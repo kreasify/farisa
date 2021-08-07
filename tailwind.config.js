ae375ae1a5fddb0a7df9b93bdc795e9d53ddda3d
Stackbit Code Editor <projects@stackbit.com>
2021-08-07 11:41:59 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -290,7 +290,7 @@ module.exports = {
 			inset: ['hover', 'group-hover', 'before', 'after'],
 			lineHeight: ['after'],
 			margin: ['first', 'last', 'before', 'after'],
-            opacity: ['active'],
+            opacity: ['before', 'after'],
 			outline: ['hover'],
 			padding: ['before', 'after', 'first', 'last'],
 			placeholderColor: ['hover', 'active'],
