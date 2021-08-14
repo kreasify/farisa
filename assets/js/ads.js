6d523ce96dbc17c8efbcae5407ac788bd6e96923
Stackbit Code Editor <projects@stackbit.com>
2021-08-14 17:30:34 +0000

    ads.js: changed

---
+++ assets/js/ads.js
@@ -0,0 +1,14 @@
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('ads', () => ({
+            users: [],
+            init() {
+                const person = async () => {
+                    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+                    if (! response.ok) alert(`Something went wrong: ${response.status} - ${response.statusText}`)
+                    data = await response.json();
+                    this.users = data;
+                }
+                return person;
+            }
+        }))
+    })
