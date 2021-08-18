3627bf043f3f7dc5b27a410a5714dae7f7a81b56
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 17:40:31 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -12,6 +12,10 @@ document.addEventListener('alpine:init', () => {
             let wrapper = this.$refs.related;
 
         wrapper.insertAdjacentHTML('beforeend', `
+        <template x-for="color in users">
+            <li x-text="color.name + color.email"></li>
+        </template>
+
         <div id="nav__search" class="nav__search search search__desktop">
             <div class="search__box">
                 <div class="search__form">
