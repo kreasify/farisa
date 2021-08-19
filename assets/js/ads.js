7b00cbe9d05d68b2f5305e277ddce52f3b5c7efa
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 08:28:02 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -19,7 +19,7 @@ document.addEventListener('alpine:init', () => {
 
         wrapper.insertAdjacentHTML('beforeend', `
         <div class="related__ads pt-6">
-            <div class="grid grid-cols-12 lg:grid-cols-10 gap-3 lg:gap-6">
+            <div class="ads grid grid-cols-12 lg:grid-cols-10 gap-3 lg:gap-6">
                 <template x-for="(item, index) in products">
                 <div class="ads-item col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
                     <div class="ads-item__image relative bg-body-primary">
