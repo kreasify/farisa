78e2c2f0764d79076310f9a2e0e053c3e4fc8e63
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 23:54:26 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -25,7 +25,29 @@ document.addEventListener('alpine:init', () => {
 
         wrapper.insertAdjacentHTML('beforeend', `
         <template x-for="(product, index) in allProducts()" :key="index">
-
+        <template x-if="index == 0 || index == 5 || index == 1 || index == 2 || index == 6">
+        <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
+            <div class="ads-item__image relative bg-body-primary">
+                <a class="ads-item__link" x-bind:href="product.url">
+                    <div class="responsive aspect-w-5 aspect-h-6">
+                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="product.image">
+                    </div>
+                </a>
+            </div>
+            <template x-if="product.title">
+            <div class="ads-item__info py-3 px-2">
+                <a class="ads-item__link" x-bind:href="product.url">
+                    <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="product.title"></h3>
+                </a>
+                <template x-if="product.price">
+                <p class="ads-item__price mb-3">
+                    <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="product.price"></span>
+                </p>
+                </template>
+            </div>
+            </template>
+        </div>
+        </template>
         <template x-if="index == 3 || index == 7 || index == 4 || index == 8 || index == 9">
         <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
             <div class="ads-item__image relative bg-body-primary">
