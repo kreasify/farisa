76864c11ce35ef5f6333e1c69c1ce7682cc89614
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 23:36:47 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -25,7 +25,7 @@ document.addEventListener('alpine:init', () => {
 
         wrapper.insertAdjacentHTML('beforeend', `
         <template x-for="(product, index) in allProducts()" :key="index">
-        <template x-if="index == 1 || index == 2 || index == 3 || index == 6">
+        <template x-if="index == 0 || index == 1 || index == 2 || index == 3 || index == 6">
         <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
             <div class="ads-item__image relative bg-body-primary">
                 <a class="ads-item__link" x-bind:href="product.url">
