c666bfa67008db4c4367959ecd52e894b52356d5
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 13:06:05 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -22,20 +22,20 @@ document.addEventListener('alpine:init', () => {
         <template x-for="(item, index) in products">
         <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
             <div class="ads-item__image relative bg-body-primary">
-                <a class="ads-item__link" x-bind:href="item.url">
+                <a class="ads-item__link" x-bind:href="product.url">
                     <div class="responsive aspect-w-5 aspect-h-6">
-                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="item.image">
+                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="product.image">
                     </div>
                 </a>
             </div>
-            <template x-if="item.title">
+            <template x-if="product.title">
             <div class="ads-item__info py-3 px-2">
-                <a class="ads-item__link" x-bind:href="item.url">
-                    <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="item.title"></h3>
+                <a class="ads-item__link" x-bind:href="product.url">
+                    <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="product.title"></h3>
                 </a>
-                <template x-if="item.price">
+                <template x-if="product.price">
                 <p class="ads-item__price mb-3">
-                    <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="item.price"></span>
+                    <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="product.price"></span>
                 </p>
                 </template>
             </div>
