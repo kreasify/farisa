c15e5e7615fb70c4f0b8661c1b10fe8473651b2f
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 08:27:52 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -21,21 +21,21 @@ document.addEventListener('alpine:init', () => {
         <div class="related__ads pt-6">
             <div class="grid grid-cols-12 lg:grid-cols-10 gap-3 lg:gap-6">
                 <template x-for="(item, index) in products">
-                <div class="feed-item col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
-                    <div class="feed-item__image relative bg-body-primary">
-                        <a class="feed-item__link" x-bind:href="item.url">
+                <div class="ads-item col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
+                    <div class="ads-item__image relative bg-body-primary">
+                        <a class="ads-item__link" x-bind:href="item.url">
                             <div class="responsive aspect-w-5 aspect-h-6">
                                 <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="item.image">
                             </div>
                         </a>
                     </div>
                     <template x-if="item.title">
-                    <div class="feed-item__info py-3 px-2">
-                        <a class="feed-item__link" x-bind:href="item.url">
-                            <h3 class="feed-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="item.title"></h3>
+                    <div class="ads-item__info py-3 px-2">
+                        <a class="ads-item__link" x-bind:href="item.url">
+                            <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="item.title"></h3>
                         </a>
                         <template x-if="item.price">
-                        <p class="feed-item__price mb-3">
+                        <p class="ads-item__price mb-3">
                             <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="item.price"></span>
                         </p>
                         </template>
