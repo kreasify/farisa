1fd55a50ca3547f40fbcb919c755b3c76b9be9e2
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 00:22:56 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -13,7 +13,7 @@ document.addEventListener('alpine:init', () => {
             // let produk = `${person.map((product, index) => product.summary = '<li>' + product.name + product.email + '</li>')}`;
 
             return person;
-            return wrapper;
+            
         },
         related() {
                         let wrapper = this.$refs.related;
