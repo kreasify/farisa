eba5dd188863cb140249a4b7c10ca8e0f22e57bc
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 13:44:34 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -10,7 +10,7 @@ document.addEventListener('alpine:init', () => {
                 data = await response.json();
                 this.products = data.items;
             };
-            let allProducts = this.related;
+            let allProducts = [...this.related, ...getProducts];
 
 
             this.relatedAd();
