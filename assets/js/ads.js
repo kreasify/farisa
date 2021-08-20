c29f78d5df5fcacc1e52841ff090eb8c241240de
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 23:12:55 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -115,7 +115,7 @@ document.addEventListener('alpine:init', () => {
                         </div>
                     </a>
                 </div>
-                <p></p>
+                <p x-text=""></p>
                 <template x-if="adFirst.title">
                 <div class="ads-item__info py-3 px-2">
                     <a class="ads-item__link" x-bind:href="adFirst.url">
