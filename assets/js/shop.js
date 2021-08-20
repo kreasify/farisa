7fedb353f49819e490d26c8df955cb8b86eb2330
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 11:32:25 +0000

    shop.js: changed

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -108,7 +108,7 @@ document.addEventListener('alpine:init', () => {
             const wrapper = document.querySelector(".card-item-1");
 
         wrapper.insertAdjacentHTML('afterend', `
-        <template x-for="(product, index) in $store.shop.products" :key="product.title">
+        <template x-for="(product, index) in products" :key="product.title">
         <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
             <div class="ads-item__image relative bg-body-primary">
                 <a class="ads-item__link" x-bind:href="product.url">
