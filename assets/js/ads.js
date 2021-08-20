cf994d97a5e19797aec2d5e645cf8b5ef760afe5
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 16:30:02 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -68,7 +68,7 @@ document.addEventListener('alpine:init', () => {
             // let wrapper = this.$refs.related1;
             // const wrapper = document.querySelector(".related-item-0");
 
-            wrapper.insertAdjacentHTML('beforebegin', `
+            wrapper.insertAdjacentHTML(position, `
             <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
                 <div class="ads-item__image relative bg-body-primary">
                     <a class="ads-item__link" x-bind:href="adFirst.url">
