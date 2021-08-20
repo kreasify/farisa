bcf6089934760fdc14c710dbe8523e8d63c37ad2
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 10:26:17 +0000

    shop.js: changed

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -89,7 +89,7 @@ document.addEventListener('alpine:init', () => {
             // console.log(this.carts)
         },
         products: [],
-        adCard: { title: null, url: null},
+        adCard: { title: null, url: null, image: null, },
         mycard: "liiii",
         init() {
             const getProducts = async () => {
