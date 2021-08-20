c38f4ccded592e940ef665062c33feb2fa756ca1
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 06:43:36 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -55,11 +55,11 @@ document.addEventListener('alpine:init', () => {
             <div class="ads-item__image relative bg-body-primary">
                 <a class="ads-item__link" x-bind:href="adProducturl">
                     <div class="responsive aspect-w-5 aspect-h-6">
-                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="product.image">
+                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="adProductimage">
                     </div>
                 </a>
             </div>
-            <template x-if="product.title">
+            <template x-if="adProducttitle">
             <div class="ads-item__info py-3 px-2">
                 <a class="ads-item__link" x-bind:href="product.url">
                     <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="product.title + index"></h3>
