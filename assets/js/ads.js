62c3d2cc6436ebf19f2c6cb91e83aaeaa2c42d06
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 06:43:43 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -53,13 +53,13 @@ document.addEventListener('alpine:init', () => {
         wrapper.insertAdjacentHTML('afterend', `
         <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
             <div class="ads-item__image relative bg-body-primary">
-                <a class="ads-item__link" x-bind:href="adProducturl">
+                <a class="ads-item__link" x-bind:href="product.url">
                     <div class="responsive aspect-w-5 aspect-h-6">
-                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="adProductimage">
+                        <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="product.image">
                     </div>
                 </a>
             </div>
-            <template x-if="adProducttitle">
+            <template x-if="product.title">
             <div class="ads-item__info py-3 px-2">
                 <a class="ads-item__link" x-bind:href="product.url">
                     <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="product.title + index"></h3>
