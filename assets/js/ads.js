5be6bb69bc0cb68b8c457f218883bbee1ea2e5b1
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 23:15:14 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -25,7 +25,7 @@ document.addEventListener('alpine:init', () => {
             const wrapper_2_after = document.querySelector(".related-item-4");
 
             // this.relatedAd();
-            if (this.related[].id == 1 ) {
+            if (this.related[0].id == 1 ) {
                 this.myIndex = 1;
             }
             if (wrapper_2) {
