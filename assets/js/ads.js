e944a7802513b0e2b66ccfa2ea00e343b9ee7a2d
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 13:23:58 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -120,20 +120,20 @@ document.addEventListener('alpine:init', () => {
             wrapper.insertAdjacentHTML('afterend', `
             <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
                 <div class="ads-item__image relative bg-body-primary">
-                    <a class="ads-item__link" x-bind:href="adSecond.url">
+                    <a class="ads-item__link" x-bind:href="adThird.url">
                         <div class="responsive aspect-w-5 aspect-h-6">
-                            <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="adSecond.image">
+                            <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="adThird.image">
                         </div>
                     </a>
                 </div>
-                <template x-if="adSecond.title">
+                <template x-if="adThird.title">
                 <div class="ads-item__info py-3 px-2">
-                    <a class="ads-item__link" x-bind:href="adSecond.url">
-                        <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="adSecond.title"></h3>
+                    <a class="ads-item__link" x-bind:href="adThird.url">
+                        <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="adThird.title"></h3>
                     </a>
-                    <template x-if="adSecond.price">
+                    <template x-if="adThird.price">
                     <p class="ads-item__price mb-3">
-                        <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="adSecond.price"></span>
+                        <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="adThird.price"></span>
                     </p>
                     </template>
                 </div>
