7d9be87fd2902ac357483aab9d118071b7df58fe
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 13:00:54 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -50,6 +50,7 @@ document.addEventListener('alpine:init', () => {
         
             return wrapper;
         },
+        
         ad() {
             // let wrapper = this.$refs.related1;
             const wrapper = document.querySelector(".card-item-1");
