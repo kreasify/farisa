f89b35e8b36dbf6e2c75588975b8ab034aa99ecf
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 16:55:19 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -30,9 +30,6 @@ document.addEventListener('alpine:init', () => {
             if (wrapper_2) {
                 this.myIndex = 2;
             }
-            if (wrapper_2_after) {
-                this.myIndex = 3;
-            }
 
             this.adCard(wrapper_0, 'afterend');
 
