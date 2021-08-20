390b37079e73cbd2eb8310bb6411a8bfd35549ae
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 10:35:12 +0000

    shop.js: changed

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -105,7 +105,7 @@ document.addEventListener('alpine:init', () => {
                 
             };
             this.ad();
-            $nextTick(() => this.mycard = getProducts[0].title );
+            this.$nextTick(() => this.mycard = getProducts[0].title );
            
             return getProducts;
         },
