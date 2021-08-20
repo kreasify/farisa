e037e3a296772cee1e55197eb6abf3f2e8994571
Stackbit Code Editor <projects@stackbit.com>
2021-08-20 15:40:10 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -73,7 +73,7 @@ document.addEventListener('alpine:init', () => {
                 <template x-if="adFirst.title">
                 <div class="ads-item__info py-3 px-2">
                     <a class="ads-item__link" x-bind:href="adFirst.url">
-                        <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="products[$index].title"></h3>
+                        <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="products[index].title"></h3>
                     </a>
                     <template x-if="adFirst.price">
                     <p class="ads-item__price mb-3">
