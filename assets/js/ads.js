2edf1cc5d3c14bda7928cd1b613680e2f5d17574
Stackbit Code Editor <projects@stackbit.com>
2021-08-21 02:55:23 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -201,6 +201,32 @@ document.addEventListener('alpine:init', () => {
             
             return wrapper;
         },
-        
+        adFourth(wrapper) {
+            wrapper.insertAdjacentHTML('afterend', `
+            <div class="ads-item relative col-span-6 md:col-span-3 lg:col-span-2 transition duration-500 ease-ease hover:shadow-lg">
+                <div class="ads-item__image relative bg-body-primary">
+                    <a class="ads-item__link" x-bind:href="adFourth.url">
+                        <div class="responsive aspect-w-5 aspect-h-6">
+                            <img class="absolute w-full h-full left-0 top-0 object-cover blur-up lazyloaded" x-bind:src="adFourth.image">
+                        </div>
+                    </a>
+                </div>
+                <template x-if="adFourth.title">
+                <div class="ads-item__info py-3 px-2">
+                    <a class="ads-item__link" x-bind:href="adFourth.url">
+                        <h3 class="ads-item__title text-sm lg:text-base text-heading font-heading font-medium mb-1" x-text="adFourth.title"></h3>
+                    </a>
+                    <template x-if="adFourth.price">
+                    <p class="ads-item__price mb-3">
+                        <span class="text-heading-primary text-xs lg:text-sm font-medium" x-text="adFourth.price"></span>
+                    </p>
+                    </template>
+                </div>
+                </template>
+            </div>
+            `);
+            
+            return wrapper;
+        },
     }))
 })
