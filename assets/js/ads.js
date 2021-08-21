da348d297d62a97a6a07dd5f7ec79114a33eb4cf
Stackbit Code Editor <projects@stackbit.com>
2021-08-21 04:10:44 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -142,7 +142,7 @@ document.addEventListener('alpine:init', () => {
                     </a>
                 </div>
                 <div class="absolute top-0 left-0 w-full h-8 flex items-center justify-end pr-1">
-                    <span class="text-xs leading-6 text-body-text bg-body opacity-50 rounded-sm px-1">Ad by Vitoko</span>
+                    <span class="text-xs leading-5 text-body-text bg-body opacity-50 rounded-sm px-1">Ad by Vitoko</span>
                 </div>
                 <template x-if="adFirst.title">
                 <div class="ads-item__info py-3 px-2">
