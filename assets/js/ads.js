9e0d1487d203450a9ee03f9a63bf09f816d30b48
Stackbit Code Editor <projects@stackbit.com>
2021-08-21 02:45:50 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -22,7 +22,7 @@ document.addEventListener('alpine:init', () => {
             const wrapper_1 = document.querySelector(".related-item-2");
             const wrapper_2 = document.querySelector(".related-item-4");
             const feed_1 = document.querySelectorAll(".feed-item-4");
-            const feed_1 = document.querySelectorAll(".feed-item-4");
+            const feed_2 = document.querySelectorAll(".feed-item-8");
             let i;
 
             if (feed_1) {
