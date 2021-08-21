0b05257922e072ef686c62f22a5b2d60409f5a7b
Stackbit Code Editor <projects@stackbit.com>
2021-08-21 02:47:55 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -42,6 +42,7 @@ document.addEventListener('alpine:init', () => {
                     this.adFirst(feed_3[i]);
                 }
             }
+            
             if (wrapper_0) {
             this.adFirst(wrapper_0);
             }
