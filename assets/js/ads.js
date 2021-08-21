4af970306a6ef0dc49d5a111be69d316d2d92882
Stackbit Code Editor <projects@stackbit.com>
2021-08-21 04:19:45 +0000

    ads.js: changed

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -156,7 +156,7 @@ document.addEventListener('alpine:init', () => {
                     </template>
                     <p class="mb-2">
                         <span class="text-xs text-body bg-body-text opacity-80 rounded-sm px-1">Iklan</span>
-                        <span>https://hijacket.site</span>
+                        <span class="text-xs text-body-text-secondary pl-2">https://hijacket.site</span>
                     </p>
                 </div>
                 </template>
