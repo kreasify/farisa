36ebc322ca4a6a1e2260400dbbe1c32c655b1c3f
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 12:47:44 +0000

    checkout.js: changed

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -64,7 +64,7 @@ document.addEventListener('alpine:init', () => {
                     element: (item, data) => {
                         // Modify Results Item Content
                         item.innerHTML = `
-                        <span class="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+                        <span class="truncate">
                         ${data.match}
                         </span>`;
                     },
