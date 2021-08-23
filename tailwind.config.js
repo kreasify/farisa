526054106f96973bcf26794a8edbd6f33533e654
Stackbit Code Editor <projects@stackbit.com>
2021-08-23 10:16:35 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
 				'primary-text': 'var(--primary-text)',
 				secondary: 'var(--secondary)',
 				'secondary-text': 'var(--secondary-text)',
+                'action-color': 'var(--secondary-text)',
 				body: 'var(--body)',
 				'body-primary': 'var(--body-primary)',
 				'body-secondary': 'var(--body-secondary)',
