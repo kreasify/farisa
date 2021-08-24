2ed7fe4c871e9b612d547172dbe4093f453d450b
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 08:16:53 +0000

    checkout.js: changed

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -130,7 +130,7 @@ document.addEventListener('alpine:init', () => {
             const district = ', Kec. ' + this.destination.split(',')[1] + ', ' + this.destination.split(',')[2] + ', ' + this.destination.split(',')[3];
             const address = `*Alamat*%20%20%20%20%3A%20${this.address}%20${district}%20${this.postcode}%0A`;
             const pembayaran = `*Pembayaran*%20%20%3A%20${this.paymentNumber}%20a/n%20${this.paymentAccount}%20(${this.payment})%0A`;
-            const waktu = `*Catatan*%20%20%3A%20${this.note}%0A%0A`;
+            const waktu = `*Waktu*%20%20%3A%20${this.note}%0A%0A`;
             const catatan = `*Catatan*%20%20%3A%20${this.note}%0A%0A`;
             const produk = `${products.map((product, index) => product.summary = '%0A*' + product.qty + 'x*%20' + product.name + '%20*' + product.size + '*%20%0A_@' + this.localPrice(product.price) + '_%20%20%3D%3D%3E%20%20%20%20%20%20%20%20%20' + this.localPrice(product.price * product.qty)).join('%0A')}`;
             const product_list = `Rincian%20Pesanan%2C%0A_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_${produk}%0A_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%20_%2B%0A`;
