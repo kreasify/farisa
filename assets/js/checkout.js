bf85dadf118f6b9e02a8d607f39d26f7e2eed102
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 10:42:52 +0000

    checkout.js: changed

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -5,7 +5,7 @@ document.addEventListener('alpine:init', () => {
         destination: '',
         couriers: [],
         destinationName: '',
-        
+        originId
         fetchCourier() {
             if (this.destination !== '') {
                 fetch(`https://ongkir-106.netlify.app/api/${this.destination.split(',')[0]}`)
